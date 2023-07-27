d46c061bee438bb64dc1381038414a2fdb2d7d55
beimingxianyu <2916826848@qq.com>
2023-07-27 15:56:43 +0800

    complete MM::RenderSystem::RenderResourceTexture

--- engine/source/runtime/function/render/RenderResourceTexture.cpp
+++ engine/source/runtime/function/render/RenderResourceTexture.cpp
@@ -2,8 +2,192 @@
 // Created by beimingxianyu on 23-7-1.
 //
 
-#include "RenderResourceTexture.h"
+#include "runtime/function/render/RenderResourceTexture.h"
 
-namespace MM {
-namespace RenderSystem {}  // namespace RenderSystem
-}  // namespace MM
+#include "runtime/function/render/vk_engine.h"
+
+MM::RenderSystem::RenderResourceTexture::RenderResourceTexture(
+    MM::RenderSystem::AllocatedImage* allocated_image,
+    MM::RenderSystem::Sampler&& sampler,
+    MM::RenderSystem::ImageView&& image_view)
+    : allocated_image_(allocated_image),
+      sampler_(std::move(sampler)),
+      image_view_(std::move(image_view)) {
+  if (allocated_image_ == nullptr || !allocated_image_->IsValid()) {
+    MM_LOG_ERROR("The input parameter allocated_image is invalid.");
+    allocated_image_ = nullptr;
+    sampler_.Reset();
+    image_view_.Release();
+    return;
+  }
+
+  if (sampler_.IsValid()) {
+    MM_LOG_ERROR("The input parameter sampler is invalid.");
+    allocated_image_ = nullptr;
+    sampler_.Reset();
+    image_view_.Release();
+    return;
+  }
+
+  if (image_view_.IsValid()) {
+    MM_LOG_ERROR("The input parameter image_view is invalid.");
+    allocated_image_ = nullptr;
+    sampler_.Reset();
+    image_view_.Release();
+    return;
+  }
+}
+
+MM::RenderSystem::RenderResourceTexture::RenderResourceTexture(
+    MM::RenderSystem::AllocatedImage* allocated_image,
+    const MM::RenderSystem::SamplerCreateInfo& sampler_create_info,
+    const MM::RenderSystem::ImageViewCreateInfo& image_view_create_info,
+    VkAllocationCallbacks* sampler_allocator,
+    VkAllocationCallbacks* image_view_allocator)
+    : allocated_image_(allocated_image), sampler_(), image_view_() {
+  if (allocated_image_ == nullptr || !allocated_image_->IsValid()) {
+    MM_LOG_ERROR("The input parameter allocated_image is invalid.");
+    return;
+  }
+
+  sampler_ = Sampler{allocated_image_->GetRenderEnginePtr()->GetDevice(),
+                     sampler_allocator, sampler_create_info};
+  if (!sampler_.IsValid()) {
+    allocated_image_ = nullptr;
+    MM_LOG_ERROR("Failed to create sampler_.");
+    return;
+  }
+
+  image_view_ = ImageView{allocated_image_->GetRenderEnginePtr()->GetDevice(),
+                          image_view_allocator, image_view_create_info};
+  if (!image_view_.IsValid()) {
+    allocated_image_ = nullptr;
+    sampler_.Reset();
+    MM_LOG_ERROR("Failed to create image_view_.");
+    return;
+  }
+}
+
+MM::RenderSystem::RenderResourceTexture::RenderResourceTexture(
+    MM::RenderSystem::AllocatedImage* allocated_image,
+    const VkSamplerCreateInfo& vk_sampler_create_info,
+    const VkImageViewCreateInfo& vk_image_view_create_info,
+    VkAllocationCallbacks* sampler_allocator,
+    VkAllocationCallbacks* image_view_allocator) {
+  if (allocated_image_ == nullptr || !allocated_image_->IsValid()) {
+    MM_LOG_ERROR("The input parameter allocated_image is invalid.");
+    return;
+  }
+
+  sampler_ = Sampler{allocated_image_->GetRenderEnginePtr()->GetDevice(),
+                     sampler_allocator, vk_sampler_create_info};
+  if (!sampler_.IsValid()) {
+    allocated_image_ = nullptr;
+    MM_LOG_ERROR("Failed to create sampler_.");
+    return;
+  }
+
+  image_view_ = ImageView{allocated_image_->GetRenderEnginePtr()->GetDevice(),
+                          image_view_allocator, vk_image_view_create_info};
+  if (!image_view_.IsValid()) {
+    allocated_image_ = nullptr;
+    sampler_.Reset();
+    MM_LOG_ERROR("Failed to create image_view_.");
+    return;
+  }
+}
+
+MM::RenderSystem::RenderResourceTexture::RenderResourceTexture(
+    MM::RenderSystem::RenderResourceTexture&& other) noexcept
+    : allocated_image_(other.allocated_image_),
+      sampler_(std::move(other.sampler_)),
+      image_view_(std::move(other.image_view_)) {
+  other.allocated_image_ = nullptr;
+}
+
+MM::RenderSystem::RenderResourceTexture&
+MM::RenderSystem::RenderResourceTexture::operator=(
+    MM::RenderSystem::RenderResourceTexture&& other) noexcept {
+  if (std::addressof(other) == this) {
+    return *this;
+  }
+
+  allocated_image_ = other.allocated_image_;
+  sampler_ = std::move(other.sampler_);
+  image_view_ = std::move(other.image_view_);
+
+  other.allocated_image_ = nullptr;
+
+  return *this;
+}
+
+const MM::RenderSystem::ImageDataInfo&
+MM::RenderSystem::RenderResourceTexture::GetImageDataInfo() const {
+  return allocated_image_->GetImageDataInfo();
+}
+
+const MM::RenderSystem::SamplerCreateInfo&
+MM::RenderSystem::RenderResourceTexture::GetSamplerCreateInfo() const {
+  return sampler_.GetSamplerCreateInfo();
+}
+
+const MM::RenderSystem::ImageViewCreateInfo&
+MM::RenderSystem::RenderResourceTexture::GetImageViewCreateInfo() const {
+  return image_view_.GetImageViewCreateInfo();
+}
+
+MM::RenderSystem::AllocatedImage&
+MM::RenderSystem::RenderResourceTexture::GetAllocatedImage() {
+  return *allocated_image_;
+}
+
+const MM::RenderSystem::AllocatedImage&
+MM::RenderSystem::RenderResourceTexture::GetAllocatedImage() const {
+  return *allocated_image_;
+}
+
+MM::RenderSystem::Sampler& MM::RenderSystem::RenderResourceTexture::GetSampler()
+    const {
+  return sampler_;
+}
+
+MM::RenderSystem::ImageView&
+MM::RenderSystem::RenderResourceTexture::GetImageView() {
+  return image_view_;
+}
+
+const MM::RenderSystem::ImageView&
+MM::RenderSystem::RenderResourceTexture::GetImageView() const {
+  return image_view_;
+}
+
+VkImage MM::RenderSystem::RenderResourceTexture::GetImage() {
+  return allocated_image_->GetImage();
+}
+
+const VkImage_T* MM::RenderSystem::RenderResourceTexture::GetImage() const {
+  return allocated_image_->GetImage();
+}
+
+VkSampler MM::RenderSystem::RenderResourceTexture::GetVkSampler() const {
+  return sampler_.GetVkSampler();
+}
+
+VkImageView MM::RenderSystem::RenderResourceTexture::GetVkImageView() {
+  return image_view_.GetVkImageView();
+}
+
+const VkImageView_T* MM::RenderSystem::RenderResourceTexture::GetVkImageView()
+    const {
+  return image_view_.GetVkImageView();
+}
+
+bool MM::RenderSystem::RenderResourceTexture::IsValid() const {
+  return allocated_image_ != nullptr;
+}
+
+void MM::RenderSystem::RenderResourceTexture::Reset() {
+  allocated_image_ = nullptr;
+  sampler_.Reset();
+  image_view_.Release();
+}
--- engine/source/runtime/function/render/RenderResourceTexture.h
+++ engine/source/runtime/function/render/RenderResourceTexture.h
@@ -1,8 +1,7 @@
 #pragma once
 
-#include "runtime/function/render/RenderResourceDataManager.h"
+#include "runtime/function/render/AllocatedImage.h"
 #include "runtime/function/render/vk_type_define.h"
-#include "vk_engine.h"
 
 namespace MM {
 namespace RenderSystem {
@@ -10,23 +9,60 @@ class RenderEngine;
 
 class RenderResourceTexture {
  public:
-  using RenderResourceHandler = RenderResourceDataManagerImp::HandlerType;
-
- public:
   RenderResourceTexture() = default;
   ~RenderResourceTexture() = default;
+  RenderResourceTexture(AllocatedImage* allocated_image, Sampler&& sampler,
+                        ImageView&& image_view);
+  RenderResourceTexture(AllocatedImage* allocated_image,
+                        const SamplerCreateInfo& sampler_create_info,
+                        const ImageViewCreateInfo& image_view_create_info,
+                        VkAllocationCallbacks* sampler_allocator,
+                        VkAllocationCallbacks* image_view_allocator);
+  RenderResourceTexture(AllocatedImage* allocated_image,
+                        const VkSamplerCreateInfo& vk_sampler_create_info,
+                        const VkImageViewCreateInfo& vk_image_view_create_info,
+                        VkAllocationCallbacks* sampler_allocator,
+                        VkAllocationCallbacks* image_view_allocator);
   RenderResourceTexture(const RenderResourceTexture& other) = delete;
-  RenderResourceTexture(RenderResourceTexture&& other) noexcept = default;
+  RenderResourceTexture(RenderResourceTexture&& other) noexcept;
   RenderResourceTexture& operator=(const RenderResourceTexture& other) = delete;
-  RenderResourceTexture& operator=(RenderResourceTexture&& other) noexcept =
-      default;
+  RenderResourceTexture& operator=(RenderResourceTexture&& other) noexcept;
+
+ public:
+  const ImageDataInfo& GetImageDataInfo() const;
+
+  const SamplerCreateInfo& GetSamplerCreateInfo() const;
+
+  const ImageViewCreateInfo& GetImageViewCreateInfo() const;
+
+  AllocatedImage& GetAllocatedImage();
+
+  const AllocatedImage& GetAllocatedImage() const;
+
+  Sampler& GetSampler() const;
+
+  ImageView& GetImageView();
+
+  const ImageView& GetImageView() const;
+
+  VkImage GetImage();
+
+  const VkImage_T* GetImage() const;
+
+  VkSampler GetVkSampler() const;
+
+  VkImageView GetVkImageView();
+
+  const VkImageView_T* GetVkImageView() const;
+
+  bool IsValid() const;
+
+  void Reset();
 
  private:
-  RenderEngine* render_engine_{nullptr};
-  RenderResourceHandler texture_handler_{};
-  AllocatedImage* image_{nullptr};
-  ImageBindData image_bind_data_{};
+  AllocatedImage* allocated_image_{nullptr};
+  mutable Sampler sampler_{};
+  ImageView image_view_{};
 };
-
 }  // namespace RenderSystem
 }  // namespace MM
