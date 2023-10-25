2384da417c51bcef2f13fcc46bbf648865941257
beimingxianyu <2916826848@qq.com>
2023-10-25 10:53:33 +0800

    fix MM::RenderSystem::CommandTask

--- engine/source/runtime/function/render/CommandTask.h
+++ engine/source/runtime/function/render/CommandTask.h
@@ -52,15 +52,6 @@ class CommandTask {
   Result<Nil, ErrorResult> Merge(
       const std::vector<CommandTaskID>& sub_command_task_IDs);
 
-  template <typename... CommandTasks>
-  Result<Nil, ErrorResult> Merge(CommandTasks&&... command_tasks);
-
-  template <typename... CommandTasks>
-  Result<Nil, ErrorResult> IsPreTaskTo(CommandTasks&&... command_tasks);
-
-  template <typename... CommandTasks>
-  Result<Nil, ErrorResult> IsPostTaskTo(CommandTasks&&... command_tasks);
-
   bool HaveSubCommandTasks() const;
 
   void AddCrossTaskFLowSyncRenderResourceIDs(
@@ -93,128 +84,14 @@ class CommandTask {
   CommandTaskID command_task_ID_{0};
   CommandType command_type_{CommandType::UNDEFINED};
   std::vector<TaskType> commands_{};
+  
   mutable std::vector<CommandTaskID> pre_tasks_{};
   mutable std::vector<CommandTaskID> post_tasks_{};
-
   mutable std::vector<CommandTaskID> sub_tasks_{};
+
   bool is_sub_task_{false};
   bool is_async_record_{false};
   std::vector<RenderResourceDataID> cross_task_flow_sync_render_resource_IDs_;
 };
-
-template <typename... CommandTasks>
-Result<Nil, ErrorResult> CommandTask::Merge(CommandTasks&&... command_tasks) {
-  bool input_parameters_is_right = true;
-  ((input_parameters_is_right &= (&command_tasks != this)), ...);
-  if (!input_parameters_is_right) {
-    MM_LOG_ERROR(
-        "The predecessor of an object of type MM::RenderSystem::CommandTask "
-        "cannot be itself.");
-    return ResultE<>{ErrorCode::INPUT_PARAMETERS_ARE_INCORRECT};
-  }
-  ((input_parameters_is_right &= (task_flow_ == command_tasks.task_flow_)),
-   ...);
-  if (!input_parameters_is_right) {
-    MM_LOG_ERROR(
-        "Two MM::RenderSystem::CommandTask's task_flow_ are not equal.");
-    return ResultE<>{ErrorCode::INPUT_PARAMETERS_ARE_INCORRECT};
-  }
-  // TODO Optimize the algorithm to remove this limitation.
-  ((input_parameters_is_right &= (command_tasks.pre_tasks_.size() == 0 &&
-                                  command_tasks.post_tasks_.size() == 0)),
-   ...);
-  if (!input_parameters_is_right) {
-    MM_LOG_ERROR(
-        "Two MM::RenderSystem::CommandTask's task_flow_ are not equal.");
-    return ResultE<>{ErrorCode::INPUT_PARAMETERS_ARE_INCORRECT};
-  }
-  ((input_parameters_is_right &=
-    (command_tasks.command_type_ == command_type_)),
-   ...);
-  if (!input_parameters_is_right) {
-    MM_LOG_ERROR(
-        "Two MM::RenderSystem::CommandTask's command_type_ are not equal.");
-    return ResultE<>{ErrorCode::INPUT_PARAMETERS_ARE_INCORRECT};
-  }
-  // TODO Optimize the algorithm to remove this limitation.
-  ((input_parameters_is_right &= (!command_tasks.HaveSubCommandTasks())), ...);
-  if (!input_parameters_is_right) {
-    MM_LOG_ERROR("Cannot nest merge MM::RenderSystem::CommandTask.");
-    return ResultE<>{ErrorCode::INPUT_PARAMETERS_ARE_INCORRECT};
-  }
-
-  std::uint32_t new_cross_task_flow_sync_render_resource_IDs_vector_size =
-      cross_task_flow_sync_render_resource_IDs_.size();
-  ((new_cross_task_flow_sync_render_resource_IDs_vector_size +=
-    command_tasks.cross_task_flow_sync_render_resource_IDs_.size()),
-   ...);
-  cross_task_flow_sync_render_resource_IDs_.reserve(
-      new_cross_task_flow_sync_render_resource_IDs_vector_size);
-  ((cross_task_flow_sync_render_resource_IDs_.insert(
-       cross_task_flow_sync_render_resource_IDs_.begin(),
-       command_tasks.cross_task_flow_sync_render_resource_IDs_.begin(),
-       command_tasks.cross_task_flow_sync_render_resource_IDs_.end())),
-   ...);
-
-  (task_flow_->RemoveRootTask(command_tasks), ...);
-  (sub_tasks_.emplace_back(std::move(*command_tasks.this_unique_ptr_)), ...);
-  ((command_tasks.is_sub_task_ = true), ...);
-
-  (task_flow_->RemoveTask(command_tasks), ...);
-
-  return ResultS<Nil>{};
-}
-
-template <typename... CommandTasks>
-Result<Nil, ErrorResult> CommandTask::IsPreTaskTo(
-    CommandTasks&&... command_tasks) {
-  bool input_parameters_is_right = true;
-  ((input_parameters_is_right &= (&command_tasks != this)), ...);
-  if (!input_parameters_is_right) {
-    MM_LOG_ERROR(
-        "The predecessor of an object of type MM::RenderSystem::CommandTask "
-        "cannot be itself.");
-    return ResultE<>{ErrorCode::INPUT_PARAMETERS_ARE_INCORRECT};
-  }
-  ((input_parameters_is_right &= (task_flow_ == command_tasks.task_flow_)),
-   ...);
-  if (!input_parameters_is_right) {
-    MM_LOG_ERROR(
-        "Two MM::RenderSystem::CommandTask's task_flow_ are not equal.");
-    return ResultE<>{ErrorCode::INPUT_PARAMETERS_ARE_INCORRECT};
-  }
-
-  (command_tasks.pre_tasks_.push_back(this_unique_ptr_), ...);
-  (task_flow_->RemoveRootTask(command_tasks), ...);
-  (post_tasks_.push_back(&command_tasks.this_unique_ptr_), ...);
-
-  return ResultS<Nil>{};
-}
-
-template <typename... CommandTasks>
-Result<Nil, ErrorResult> CommandTask::IsPostTaskTo(
-    CommandTasks&&... command_tasks) {
-  bool input_parameters_is_right = true;
-  ((input_parameters_is_right &= (&command_tasks != this)), ...);
-  if (!input_parameters_is_right) {
-    MM_LOG_ERROR(
-        "The post task of an object of type MM::RenderSystem::CommandTask "
-        "cannot be itself.");
-    return ResultE<>{ErrorCode::INPUT_PARAMETERS_ARE_INCORRECT};
-  }
-  ((input_parameters_is_right &= (task_flow_ == command_tasks.task_flow_)),
-   ...);
-  if (!input_parameters_is_right) {
-    MM_LOG_ERROR(
-        "Two MM::RenderSystem::CommandTask's task_flow_ are not equal.");
-    return ResultE<>{ErrorCode::INPUT_PARAMETERS_ARE_INCORRECT};
-  }
-
-  (command_tasks.post_tasks_.push_back(this_unique_ptr_), ...);
-  (pre_tasks_.push_back(&command_tasks.this_unique_ptr_), ...);
-  task_flow_->RemoveRootTask(*this);
-
-  return ResultS<Nil>{};
-}
 }  // namespace RenderSystem
 }  // namespace MM
